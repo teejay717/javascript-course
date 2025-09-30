50089b342434d7e095c9e3eb4ce715215cbf995f
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2025-10-01 00:56:22 +0800

    new

---
+++ javascript-course/scripts/12-rock-paper-scissors.js
@@ -0,0 +1,123 @@
+    
+    let scores = JSON.parse(localStorage.getItem('scores')) || {
+            wins: 0,
+            losses: 0,
+            ties: 0
+        };
+        
+    updateScoreElement();
+    /*      
+    if (!scores) {
+        scores = {
+            wins: 0,
+            losses: 0,
+            ties: 0
+        }
+    }
+*/
+
+    console.log(scores);
+
+    let isAutoPlaying = false;
+    let intervalId;
+    
+
+    function autoPlay() {
+        if (!isAutoPlaying) {
+            intervalId = setInterval(function() {
+            const playerMove = pickComputerMove();
+            playGame(playerMove)
+        }, 1000);
+        isAutoPlaying = true;
+        }
+        else {
+            clearInterval(intervalId);
+            isAutoPlaying = false;
+        }
+    }
+
+    function playGame(playerMove, wins, losses, ties) {
+        const computerChoice = pickComputerMove();
+
+        let result = '';
+        
+        if (playerMove === 'scissors') {
+        
+            if (computerChoice === 'rock') {
+                result = 'You Lose';
+            }
+            else if (computerChoice === 'paper') {
+                result = 'You Win';
+            }
+            else if (computerChoice === 'scissors') {
+                result = 'Tie';
+            }
+
+        }
+
+        else if (playerMove === 'paper') {
+
+            if (computerChoice === 'rock') {
+                result = 'You Win';
+            }
+            else if (computerChoice === 'paper') {
+                result = 'Tie';
+            }
+            else if (computerChoice === 'scissors') {
+                result = 'You Lose';
+            }
+        
+        }
+
+        else if (playerMove === 'rock') {
+
+            if (computerChoice === 'rock') {
+                result = 'Tie';
+            }
+            else if (computerChoice === 'paper') {
+                result = 'You Lose';
+            }
+            else if (computerChoice === 'scissors') {
+                result = 'You Win';
+            }  
+        }
+
+        if (result === 'You Win') {
+            scores.wins = scores.wins + 1;
+        }
+        else if (result === 'You Lose') {
+            scores.losses = scores.losses + 1; 
+        }
+        else if (result === 'Tie') {
+            scores.ties = scores.ties + 1;
+        }
+
+        localStorage.setItem('scores', JSON.stringify(scores));
+        updateScoreElement();
+
+        document.querySelector('.js-result').innerHTML = result;
+        document.querySelector('.js-moves').innerHTML = `You <img src="images/${playerMove}-emoji.png" class="move-icon"><img src="images/${computerChoice}-emoji.png" class="move-icon">Computer`;
+
+    }   
+    
+    function updateScoreElement () {
+        document.querySelector('.js-score').innerHTML = `Wins: ${scores.wins}, Losses: ${scores.losses}, Ties: ${scores.ties}`;
+    }
+
+    function pickComputerMove() {
+        const randomNumber = Math.random();
+
+        let computerChoice = ''; 
+        
+        if (randomNumber >= 0 && randomNumber < 1/3) {
+            computerChoice = 'rock';
+        } 
+        else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+            computerChoice = 'paper';
+        }
+        else if (randomNumber >= 2/3 && randomNumber < 1) {
+            computerChoice = 'scissors';
+        }
+
+    return computerChoice;
+}
---
+++ javascript-course/scripts/12-todo-list.js
@@ -0,0 +1,51 @@
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2025-9-24'
+}, {
+    name: 'wash disher',
+    dueDate: '2025-9-24'
+}];
+
+
+function renderTodoList () {
+    let todoListHTML = '';
+
+    todoList.forEach(function(todoObject, index) {
+        const { name, dueDate } = todoObject;
+        
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+        todoList.splice(${index}, 1);
+        renderTodoList();
+        
+        " class="delete-todo-button">Delete</button>`;
+        todoListHTML += html;
+    })
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+
+    const name = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+    
+    todoList.push({
+        //name: name,
+        //dueDate: dueDate
+        name,
+        dueDate
+    }
+    );
+    
+
+    inputElement.value = '';
+    renderTodoList();
+}
+
+renderTodoList();
